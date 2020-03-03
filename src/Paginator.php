b31b0d1786a33b93727dfb8ca8facde21c993f08
Dale Snowdon <dale@snowdon.dev>
2020-03-03 19:24:24 +0000

    Add basic Paginator.

--- src/Paginator.php
+++ src/Paginator.php
@@ -2,6 +2,8 @@
 
 namespace Paginator;
 
+use Traversable;
+
 class Paginator implements InterfacePaginator
 {
 
@@ -15,34 +17,71 @@ class Paginator implements InterfacePaginator
     /**
      * @var int
      */
-    private int $pageNo;
-
+    private int $perPage;
 
     /**
-     * Paginator constructor.
-     * @param InterfaceRepository   $repository
-     * @param int                   $pages          the page to request from the repository
+     * @var int
      */
-    public function __construct(InterfaceRepository $repository, int $pages = 1)
-    {
+    private int $currentPage;
+
+    /**
+     * @var string
+     */
+    private string $sort;
+
+    private ?iterable $currentElements;
+
+    /**
+     * Build paginating data structure.
+     *
+     * @param InterfaceRepository $repository data source
+     * @param int $pageSize items per page
+     * @param int $page current page number
+     * @param mixed[] $filters filters to apply on respective source
+     * @param string $sort order to sort respective data
+     */
+    public function __construct(
+        InterfaceRepository $repository,
+        int $pageSize,
+        int $page = 1,
+        array $filters = [],
+        string $sort = self::SORT_ASC
+    ) {
+        if (!$page || !$pageSize)
+            throw new \InvalidArgumentException(
+                'Paginator ' . $page
+                    ? '$pageSize'
+                    : '$page'
+                    . ' requires: int > 0'
+            );
+
         $this->repository = $repository;
-        $this->filters = [];
-        $this->pageNo = $pages;
+        $this->filters = $filters;
+        $this->perPage = $pageSize;
+        $this->currentPage = $page;
+        $this->sort = $sort;
     }
 
-    public function addFilter($filter)
-    {
-        $this->filters[] = $filter;
-    }
-
+    /**
+     * @inheritDoc
+     */
     public function elements(): iterable
     {
-        // TODO: Implement elements() method.
+        $start = ($this->currentPage() - 1) * $this->perPage + 1;
+        return $this->currentElements = $this->repository->get(
+            $start,
+            $start + $this->perPage,
+            $this->filters,
+            $this->sort
+        );
     }
 
+    /**
+     * @inheritDoc
+     */
     public function currentPage(): int
     {
-        // TODO: Implement currentPage() method.
+        return $this->currentPage;
     }
 
     /**
@@ -50,21 +89,57 @@ class Paginator implements InterfacePaginator
      */
     public function pages(): array
     {
-        // TODO: Implement pages() method.
+        return range(1, $this->getPageCount());
     }
 
+    /**
+     * @inheritDoc
+     */
     public function totalElements(): int
+    {
+        return $this->getCount();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function totalElementsOnCurrentPage(): int
+    {
+        if (!empty($this->currentElements)) {
+            if (is_array($this->currentElements)) {
+                return count($this->currentElements);
+            } else if ($this->currentElements instanceof Traversable) {
+                return count(iterator_to_array($this->currentElements));
+            }
+        }
+        return $this->isLastPage() ? $this->getSpillOver() : $this->perPage;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function totalElementsPerPage(): int
+    {
+        // TODO: Implement totalElementsPerPage() method.
+    }
+
+    protected function getPageCount()
+    {
+        return $this->getCount() / $this->perPage + (bool) $this->getSpillOver();
+    }
+
+    protected function isLastPage(): bool
+    {
+        return $this->currentPage() === count($this->pages());
+    }
+
+    protected function getCount(): int
     {
         return $this->repository->count($this->filters);
     }
 
-    public function totalElementsOnCurrentPage(): int
+    protected function getSpillOver(): int
     {
-        // TODO: Implement totalElementsOnCurrentPage() method.
-    }
-
-    public function totalElementsPerPage(): int
-    {
-        // TODO: Implement totalElementsPerPage() method.
+        return $this->getCount() % $this->perPage;
     }
 }
--- tests/PaginatorTest.php
+++ tests/PaginatorTest.php
@@ -29,20 +29,20 @@ class PaginatorTest extends TestCase
            ->method('count')
            ->willReturn($total_count);
 
-       $paginator = new Paginator($this->repository_mock);
-       $result = $paginator->totalElements();
-       $this->assertEquals($total_count, $result);
+       $paginator = new Paginator($this->repository_mock, 10);
+
+       $this->assertEquals($total_count, $paginator->totalElements());
    }
 
     public function testPagesWillReturnCorrectPageKeys()
     {
-        $pageNo = 2;
+        $pageSize = 50;
 
         $this->repository_mock
             ->method('count')
             ->willReturn(100);
 
-        $paginator = new Paginator($this->repository_mock, $pageNo);
+        $paginator = new Paginator($this->repository_mock, $pageSize);
 
         $keys = $paginator->pages();
 
@@ -52,43 +52,168 @@ class PaginatorTest extends TestCase
     }
 
 
-    public function testReturnsPageElements()
+    public function testPagesWillReturnCorrectPageKeysUnEven()
     {
-        $pageNo = 1;
+        $pageSize = 50;
 
         $this->repository_mock
-            ->expects($this->once())
+            ->method('count')
+            ->willReturn(101);
+
+        $paginator = new Paginator($this->repository_mock, $pageSize);
+
+        $keys = $paginator->pages();
+
+        $this->assertCount(3, $keys);
+        $this->assertEquals($keys[0], 1);
+        $this->assertEquals($keys[1], 2);
+        $this->assertEquals($keys[2], 3);
+    }
+
+    public function testReturnsPageElements()
+    {
+        $pageSize = 3;
+
+        $this->repository_mock
             ->method('count')
             ->willReturn(6);
+
+        $returnArray = [0, 1, 2];
 
         $this->repository_mock
             ->expects($this->once())
             ->method('get')
-            ->with(0, 2, []);
+            ->with(1, 4, [])
+            ->willReturn($returnArray);
 
-        $paginator = new Paginator($this->repository_mock, $pageNo);
+        $paginator = new Paginator($this->repository_mock, $pageSize);
 
         $elements = $paginator->elements();
-        $this->assertEquals([0, 1, 2], $elements);
+        $this->assertEquals($returnArray, $elements);
     }
 
     public function testReturnsPageElementsSecondPage()
     {
-        $pageNo = 2;
+        $pageSize = 3;
 
         $this->repository_mock
-            ->expects($this->once())
             ->method('count')
             ->willReturn(6);
+
+        $returnArray = [3, 4, 5];
 
         $this->repository_mock
             ->expects($this->once())
             ->method('get')
-            ->with(3, 5, []);
+            ->with(4, 7, [])
+            ->willReturn($returnArray);
 
-        $paginator = new Paginator($this->repository_mock, $pageNo);
+        $paginator = new Paginator($this->repository_mock, $pageSize, 2);
 
         $elements = $paginator->elements();
-        $this->assertEquals([3, 4, 5], $elements);
+        $this->assertEquals($returnArray, $elements);
+    }
+
+    public function testReturnsPageElementsFourthPage()
+    {
+        $pageSize = 5;
+
+        $this->repository_mock
+            ->method('count')
+            ->willReturn(20);
+
+        $returnArr = [16, 17, 18, 19, 20];
+
+        $this->repository_mock
+            ->expects($this->once())
+            ->method('get')
+            ->with(16, 21, [])
+            ->willReturn($returnArr);
+
+        $paginator = new Paginator($this->repository_mock, $pageSize, 4);
+
+        $elements = $paginator->elements();
+        $this->assertEquals($returnArr, $elements);
+    }
+
+    public function testReturnsPageElementsSpillOver()
+    {
+        $pageSize = 5;
+
+        $this->repository_mock
+            ->method('count')
+            ->willReturn(21);
+
+        $returnArr = [21];
+
+        $this->repository_mock
+            ->expects($this->once())
+            ->method('get')
+            ->with(21, 26, [])
+            ->willReturn($returnArr);
+
+        $paginator = new Paginator($this->repository_mock, $pageSize, 5);
+
+        $elements = $paginator->elements();
+        $this->assertEquals($returnArr, $elements);
+    }
+
+    public function testTotalElementsOnCurrentPage()
+    {
+        $pageSize = 10;
+        $currentPage = 3;
+
+        $this->repository_mock
+            ->method('count')
+            ->willReturn(100);
+
+        $paginator = new Paginator($this->repository_mock, $pageSize, $currentPage);
+
+        $this->assertEquals($pageSize, $paginator->totalElementsOnCurrentPage());
+    }
+
+    // test redundant query is not issued
+    public function testTotalElementsOnCurrentPageWillNotCallRepositoryCountGivenAPreviousCallToElements()
+    {
+        $pageSize = 10;
+        $currentPage = 3;
+
+        $this->repository_mock
+            ->method('get')
+            ->willReturn([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+        $paginator = new Paginator($this->repository_mock, $pageSize, $currentPage);
+        $paginator->elements();
+
+        $this->assertEquals( 10, $paginator->totalElementsOnCurrentPage());
+    }
+
+    public function testTotalElementsOnSpillOverPage()
+    {
+        $pageSize = 10;
+        $currentPage = 11;
+
+        $this->repository_mock
+            ->method('count')
+            ->willReturn(109);
+
+        $paginator = new Paginator($this->repository_mock, $pageSize, $currentPage);
+
+        $this->assertEquals(9, $paginator->totalElementsOnCurrentPage());
+    }
+
+    public function testElementsWillWorkWithCustomIterableType()
+    {
+        $output = new \ArrayIterator([1, 2, 3, 4]);
+        $pageSize = 4;
+        $currentPage = 1;
+
+        $this->repository_mock
+            ->method('get')
+            ->willReturn($output);
+
+        $paginator = new Paginator($this->repository_mock, $pageSize, $currentPage);
+
+        $this->assertEquals($output, $paginator->elements());
     }
 }
